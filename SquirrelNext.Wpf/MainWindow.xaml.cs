30bdef7b2ecc2107237df5695abf380ce6f0bb7b
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-11-14 22:04:24 +0200

    Prepare for first release

--- SquirrelNext.Wpf/MainWindow.xaml.cs
+++ SquirrelNext.Wpf/MainWindow.xaml.cs
@@ -1,28 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Squirrel;
 
-namespace SquirrelNext.Wpf
+namespace SquirrelNext.Wpf;
+
+public partial class MainWindow
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private const string RepositoryUrl = "https://github.com/meJevin/WPFFrameworkTest";
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        Loaded += MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        using var manager = await UpdateManager.GitHubUpdateManager(RepositoryUrl);
+        CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+    }
+
+    private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
+    {
+        using var manager = await UpdateManager.GitHubUpdateManager(RepositoryUrl);
+        var updateInfo = await manager.CheckForUpdate();
+
+        UpdateButton.IsEnabled = updateInfo.ReleasesToApply.Any();
+    }
+
+    private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+    {
+        using var manager = await UpdateManager.GitHubUpdateManager(RepositoryUrl);
+
+        try
         {
-            InitializeComponent();
+            await manager.UpdateApp();
         }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+
+        MessageBox.Show("Updated successfully!");
     }
 }
