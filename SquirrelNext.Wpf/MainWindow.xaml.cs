6fd6f5878aeb49fe125c81e4b2366d601843d8df
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-11-15 15:13:02 +0200

    Configure app settings file backup

--- SquirrelNext.Wpf/MainWindow.xaml.cs
+++ SquirrelNext.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
 using Squirrel;
 using SquirrelNext.Domain;
@@ -32,23 +35,70 @@ public partial class MainWindow
 
     private async void UpdateButton_Click(object sender, RoutedEventArgs e)
     {
-        using var manager = await UpdateManager.GitHubUpdateManager(RepoUrl);
-
-        try
-        {
-            await manager.UpdateApp();
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(ex.Message);
-        }
-
-        MessageBox.Show("Updated successfully!");
+        await Update();
+        Application.Current.Shutdown();
     }
 
     private void SumButton_OnClick(object sender, RoutedEventArgs e)
     {
         var sum = new Summator();
         MessageBox.Show(sum.Sum(3, 3).ToString());
+    }
+
+    private async Task Update()
+    {
+        using var manager = await UpdateManager.GitHubUpdateManager(RepoUrl);
+        try
+        {
+            BackupSettings("appsettings.json");
+            await manager.UpdateApp();
+            RestoreSettings("appsettings.json");
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private static void BackupSettings(string configFileName)
+    {
+        var exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        if (exeDir is null)
+        {
+            return;
+        }
+
+        var sourceFile = Path.Combine(exeDir, configFileName);
+
+        if (!File.Exists(sourceFile))
+        {
+            return;
+        }
+
+        var destFile = Path.Combine(Directory.GetParent(exeDir)!.FullName, configFileName);
+        File.Copy(sourceFile, destFile, true);
+    }
+
+    private static void RestoreSettings(string configFileName)
+    {
+        var exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        if (exeDir is null)
+        {
+            return;
+        }
+        
+        var sourceFile = Path.Combine(Directory.GetParent(exeDir)!.FullName, configFileName);
+        
+        if (!File.Exists(sourceFile))
+        {
+            return;
+        }
+
+        var destFile = Path.Combine(exeDir, configFileName);
+        
+        File.Copy(sourceFile, destFile, true);
+        File.Delete(sourceFile);
     }
 }
